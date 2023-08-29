982a50fea0065bbe5a0845d7b130c4988d919c9f
Dinesh <dineshraghavan29@gmail.com>
2023-08-29 17:08:37 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
+import ListGroup from "./components/ListGroup";
+
+const App = () => {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+};
+
+export default App;

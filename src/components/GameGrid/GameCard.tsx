f91932e3240240a535174bb742e38f5727bbb9e2
Dinesh <dineshraghavan29@gmail.com>
2023-09-03 19:00:20 +0530

    Display skeletons on loading

--- src/components/GameGrid/GameCard.tsx
+++ src/components/GameGrid/GameCard.tsx
@@ -6,7 +6,7 @@ import getCroppedImageUrl from "../../services/img-url";
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="250px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,21 +6,27 @@ import { CanceledError } from "axios";
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setisLoading(true);
     apiClient
       .get<Games>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setisLoading(false);
+        setGames(res.data.results);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setisLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;

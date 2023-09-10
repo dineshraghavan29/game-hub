6e6f49989d031da692e7342be20d86c9dbc37e5d
Dinesh <dineshraghavan29@gmail.com>
2023-09-10 20:11:49 +0530

    Highlight selected genre

--- src/components/SideMenu/GenreList.tsx
+++ src/components/SideMenu/GenreList.tsx
@@ -11,10 +11,11 @@ import getCroppedImageUrl from "../../services/img-url";
 import { Genre } from "../../hooks/Genres.type";
 
 type GenreListProps = {
+  selectedGenre: Genre | null;
   onSelectGenre: (genre: Genre) => void;
 };
 
-const GenreList = ({ onSelectGenre }: GenreListProps) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: GenreListProps) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -22,24 +23,29 @@ const GenreList = ({ onSelectGenre }: GenreListProps) => {
 
   return (
     <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              variant="link"
-              onClick={() => onSelectGenre(genre)}
-              fontSize="lg"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
+      {data.map((genre) => {
+        const isSelected = genre.id === selectedGenre?.id;
+
+        return (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                variant="link"
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                fontWeight={isSelected ? "bold" : "normal"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        );
+      })}
     </List>
   );
 };
